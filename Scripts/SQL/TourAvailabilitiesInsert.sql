67411e2463bd6207573e1791e793ba8ae030fa68
Ethan <ethanhawthorne586@gmail.com>
2024-01-18 14:44:54 +0000

    started commiting

--- Scripts/SQL/TourAvailabilitiesInsert.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO TourAvailabilities (TourAvailabilityId, TourId, AvailableFrom, AvailableTo)
-VALUES (
-  NEWID(),
-  '',
-  '2024-01-15',
-  '2024-01-20'
-);
--- Scripts/SQL/ToursInsert.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO HotelAvailabilities (HotelAvailabilityId, HotelId, AvailableFrom, AvailableTo)
-VALUES (
-  NEWID(),
-  '',
-  '2024-01-08',
-  '2024-01-28'
-);
